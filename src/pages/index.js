a84ec7dc2cee78cf08f3d221c20e752c45f4ed1a
Luis <che.luis01@gmail.com>
2019-05-01 13:19:23 -0500

    add code tag styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,32 @@
-import React from "react"
+import React, { useState, useEffect } from 'react';
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = () => {
+  let [data,setData] = useState(null)
+  useEffect(() => {
+    // Update the document title using the browser API
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(response => response.json())
+    .then(json => {
+      setData(json)
+    })
+  })
+return (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi Isaac !</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>La informacion recibida es:</p>
+    {data ? Object.keys(data).map(obj => <div key={data[obj].id}>{data[obj].title}</div>): "nada aun" }
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
-)
+  )
+}
 
 export default IndexPage
